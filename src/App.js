b5fbfe3fb9d495d4b1af3a66a64ddaf6aa566826
Shane McDonald <shanemcdonald@Shanes-Air.attlocal.net>
2022-02-17 19:03:27 -0600

    changed the promise syntax and added another state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]); //setting the state to have the movie list ready to be stored and then called
-  function fetchMoviesHandler() { //setting the fetch request
-    fetch('https://swapi.dev/api/films') //url to fetch
-    .then(response => { //the response promise
-      return response.json(); //data returned in json format
-    }).then(data => { //what we're doing with the data
+  const [isLoading, setIsLoading] = useState(false)
+  async function fetchMoviesHandler() { //setting the fetch request
+    setIsLoading(true); // changing the data response state to true, 
+    const response = await fetch('https://swapi.dev/api/films') //url to fetch, await used to automatically infer the .then(response) portion of the promise, setting it as a constant
+    const data =  await response.json(); //data returned in json format, the await taking care of the .then(response) portiong of the promise
       const transformedMovies = data.results.map((movieData) => { //mapping through the data (data.results being the actual data ("data" is a place holder))
         return {
           id: movieData.episode_id, //how we want the data to be called
@@ -19,9 +19,9 @@ function App() {
         }
       });
       setMovies(transformedMovies); //set the state (data returned) in the app
-    })
     // note the movies props in MovieList component is the state and we're using it as a prop here, so the state of the movies can be called and used as props, 
     //in this case movies are the url of json info*//> 
+    setIsLoading(false); //resetting the changed state back to original state bc we're no longer dealing with changed state
   }
 
   return (
@@ -30,10 +30,15 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found No Movies</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
 }
 
 export default App;
+// line 33, wrapped the component in curly braces to include JS code. Here, we tell the component that if state is currently loading data, show the list.
+//That is accomplished asynchronously via the movies state and map function mapping through movies state.
+//line 34, telling the app that if state 'isLoading' is active, don't render the list and show the paragraph on the page. 
--- src/Components/MoviesList.jsx
+++ src/Components/MoviesList.jsx
@@ -8,6 +8,7 @@ const MovieList = (props) => {
     <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
         <Movie
+          id={movie.ids}
           title={movie.title}
           releaseDate={movie.releaseDate}
           openingText={movie.openingText}
