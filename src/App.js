e603f28e3db34af9f1d2aef1ab05d9776761b37a
Shane McDonald <shanemcdonald@Shanes-Air.attlocal.net>
2022-02-17 21:49:58 -0600

    small practice with sending data to a backend

--- src/App.js
+++ src/App.js
@@ -1,42 +1,72 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './Components/MoviesList';
+import AddMovie from './Components/AddMovie';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]); //setting the state to have the movie list ready to be stored and then called
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  async function fetchMoviesHandler() { //setting the fetch request
+  const fetchMoviesHandler = useCallback(async () =>  { //setting the fetch request
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films') //url to fetch
+      const response = await fetch('https://react-posting-default-rtdb.firebaseio.com/movies.json') //url to fetch
       if (!response.ok) {
         throw new Error('Something Went Wrong')
       }  
       const data = await response.json(); //data returned in json format
-      const transformedMovies = data.results.map((movieData) => { //mapping through the data (data.results being the actual data ("data" is a place holder))
-          return {
-            id: movieData.episode_id, //how we want the data to be called
-            title: movieData.title, //how we want the data to be called
-            openingText: movieData.opening_crawl, //how we want the data to be called (i.e. opening_crawl will be mapped through as openingText)
-            releaseDate: movieData.release_date //how we want the data to be called
-          }
+      
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id:key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
         });
-        setMovies(transformedMovies); //set the state (data returned) in the app
+      }
+      
+      // const transformedMovies = data.results.map((movieData) => { //mapping through the data (data.results being the actual data ("data" is a place holder))
+      //   return {
+      //     id: movieData.episode_id, //how we want the data to be called
+      //     title: movieData.title, //how we want the data to be called
+      //     openingText: movieData.opening_crawl, //how we want the data to be called (i.e. opening_crawl will be mapped through as openingText)
+      //     releaseDate: movieData.release_date //how we want the data to be called
+      //   }
+      // });
+      setMovies(loadedMovies); //set the state (data returned) in the app
       // note the movies props in MovieList component is the state and we're using it as a prop here, so the state of the movies can be called and used as props, 
       //in this case movies are the url of json info*//> 
       setIsLoading(false)
     }
-       catch (error) {
+    catch (error) {
       setError(error.message);
     }
     setIsLoading(false)
+  }, [])
+  useEffect(() => { //ensures that the fetch call hapens on page load and not just on button click.
+    fetchMoviesHandler()//this is the function it affects, what we want to happen on page load
+  }, [fetchMoviesHandler,]);//what the effect depends on
+  
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-posting-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data)
   }
-
+  
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
