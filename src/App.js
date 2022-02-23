f18db4eb8f34b653dad7ba7c167ad039b261bd4e
Shane-McDonald33 <mcdonaldshane33@gmail.com>
2022-02-23 08:30:42 -0600

    added some internal notes

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]); //setting the state to have the movie list ready to be stored and then called
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);//sets state for when data is load stage, allows to work with data in between stages
+  const [error, setError] = useState(null);//setting up error calls
   const fetchMoviesHandler = useCallback(async () =>  { //setting the fetch request
-    setIsLoading(true);
-    setError(null);
+    setIsLoading(true); //sets load state to true and therefore active
+    setError(null);//tells the app that error is null while app loads unless an error activates
     try {
       const response = await fetch('https://react-posting-default-rtdb.firebaseio.com/movies.json') //url to fetch
-      if (!response.ok) {
+      if (!response.ok) {//'response.ok' checks for http return is 200 and if not then an error is called
         throw new Error('Something Went Wrong')
       }  
       const data = await response.json(); //data returned in json format
       
-      const loadedMovies = [];
-      for (const key in data) {
-        loadedMovies.push({
+      const loadedMovies = [];//sets variable to an empty array
+      for (const key in data) {//key is the id paramenter in the data
+        loadedMovies.push({//pushing to the app
           id:key,
           title: data[key].title,
           openingText: data[key].openingText,
